6b10e9dc17b53c7f37399cd440b72e0e9abddd8e
weifeng cao <weifengcao@gmail.com>
2025-09-18 11:24:22 -0700

    Add allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const allowedHosts = (process.env.ALLOWED_HOSTS ?? "")
+  .split(",")
+  .map((host) => host.trim())
+  .filter(Boolean);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +13,6 @@ export default defineConfig({
     hmr: {
       // protocol
     },
-  }
+    allowedHosts,
+  },
 });
